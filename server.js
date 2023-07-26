c408582477d3323a69ef0b5e82dd2074ffab7060
MarcinLawrenc <marcin.m.lawrenc@gmail.com>
2023-07-26 23:43:09 +0200

    added stylesheet

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const server = http.createServer((req, res) => {
     //set header content type
     res.setHeader('Content-Type', 'text/html');
 
+    res.write('<head><link rel="stylesheet" href="#"></head>')
     res.write('<p>hello, node</p>');
     res.end();
 });
 
 server.listen(3000,'localhost', () => {
-    console.log('listening on port 000');
+    console.log('listening on port 3000');
 });
